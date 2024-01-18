18d713c91dc96372b9d85200141b55bc45a1cf95
steffydominic <116252088+steffydominic@users.noreply.github.com>
2024-01-18 14:09:54 +0530

    Update MultiplicationTable.java

--- 12_01_2024/MultiplicationTable.java
+++ 12_01_2024/MultiplicationTable.java
@@ -3,9 +3,8 @@ public class MultiplicationTable {
         int n = 3;
         int m = 4;
 
-        // Print multiplication tables
         for (int i = 1; i <= n; i++) {
-            System.out.println("Multiplication table of " + i);
+            System.out.println("Table of " + i);
 
             for (int j = 1; j <= m; j++) {
                 System.out.println(i + "x" + j + " = " + (i * j));
